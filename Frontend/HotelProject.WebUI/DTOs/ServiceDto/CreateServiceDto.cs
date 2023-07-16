3fde00c974e050fbf6949a2af154ae7a21a89dec
ardailhan <ardailhan_@outlook.com>
2023-07-16 10:47:39 +0300

    added Dtos to frontend part

---
+++ Frontend/HotelProject.WebUI/DTOs/ServiceDto/CreateServiceDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.DTOs.ServiceDto
+{
+    public class CreateServiceDto
+    {
+        [Required(ErrorMessage = "Please enter service icon link ")]
+        public string ServiceIcon { get; set; }
+        [Required(ErrorMessage = "Please enter service title ")]
+        [StringLength(100, ErrorMessage = "Service title must have 100 charachters")]
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Frontend/HotelProject.WebUI/DTOs/ServiceDto/UpdateServiceDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.DTOs.ServiceDto
+{
+    public class UpdateServiceDto
+    {
+        public int ServiceID { get; set; }
+        [Required(ErrorMessage = "Please enter service icon link ")]
+        public string ServiceIcon { get; set; }
+        [Required(ErrorMessage = "Please enter service title ")]
+        [StringLength(100, ErrorMessage = "Service title must have 100 charachters")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Please enter service description ")]
+        [StringLength(500, ErrorMessage = "Service description must have 500 charachters")]
+        public string Description { get; set; }
+    }
+}
