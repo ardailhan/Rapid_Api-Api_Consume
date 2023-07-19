f9c81fcaaa26078380afff95b7fb54e69a666b18
ardailhan <ardailhan_@outlook.com>
2023-07-19 10:18:15 +0300

    booking configs on admin side

---
+++ Frontend/HotelProject.WebUI/Controllers/BookingAdminController.cs
@@ -0,0 +1,31 @@
+﻿using HotelProject.WebUI.DTOs.BookingDto;
+using HotelProject.WebUI.DTOs.ServiceDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class BookingAdminController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BookingAdminController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5269/api/Booking");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBookingDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+    }
+}
