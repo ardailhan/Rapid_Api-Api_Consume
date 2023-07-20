b8a18e327455a92b241a7ae08ea6368d20904253
ardailhan <ardailhan_@outlook.com>
2023-07-20 20:52:43 +0300

    Jwt configurations done

---
+++ JwtProject/WebAPi.Jwt/Controllers/DefaultController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebAPi.Jwt.Models;
+
+namespace WebAPi.Jwt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult TokenCreator() 
+        {
+            return Ok(new CreateToken().TokenCreate());
+        }
+        [HttpGet("[action]")]
+        public IActionResult AdminTokenCreater()
+        {
+            return Ok(new CreateToken().TokenCreateAdmin());
+        }
+        [Authorize]
+        [HttpGet("[action]")]
+        public IActionResult Test2()
+        {
+            return Ok("Welcome");
+        }
+        [Authorize(Roles = "Admin,Visitor")]
+        [HttpGet("[action]")]
+        public IActionResult Test3()
+        {
+            return Ok("Token successfully logged in..");
+        }
+    }
+}
