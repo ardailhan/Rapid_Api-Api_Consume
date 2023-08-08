eee573ecb6f5286aa0601988dd4842909b26562c
ardailhan <ardailhan_@outlook.com>
2023-08-08 11:25:31 +0300

    LinkedIn and Instagram followers done in rapid Api

---
+++ Frontend/HotelProject.WebUI/DTOs/FollowersDto/ResultLinkedInFollowersDto.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelProject.WebUI.DTOs.FollowersDto
+{
+    public class ResultLinkedInFollowersDto
+    {
+        public Data data { get; set; }
+        public class Data
+        {
+            public int followers_count { get; set; }
+        }
+    }
+}
--- Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardSubscribeCountPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardSubscribeCountPartial.cs
@@ -1,11 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.DTOs.FollowersDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelProject.WebUI.ViewComponents.Dashboard
 {
     public class _DashboardSubscribeCountPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        public async Task<IViewComponentResult> InvokeAsync()
         {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://instagram-profile1.p.rapidapi.com/getprofile/ardailhan_"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "93c7a270c3mshb757eab12d7d3d7p1cdb61jsnf69aee22abc7" },
+        { "X-RapidAPI-Host", "instagram-profile1.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                ResultInstagramFollowersDto resultInstagramFollowersDtos = JsonConvert.DeserializeObject<ResultInstagramFollowersDto>(body);
+                ViewBag.v1 = resultInstagramFollowersDtos.followers;
+                ViewBag.v2 = resultInstagramFollowersDtos.following;
+            }
+
+            var client2 = new HttpClient();
+            var request2 = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://fresh-linkedin-profile-data.p.rapidapi.com/get-linkedin-profile?linkedin_url=www.linkedin.com%2Fin%2Farda-ilhan-438b52226"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "93c7a270c3mshb757eab12d7d3d7p1cdb61jsnf69aee22abc7" },
+        { "X-RapidAPI-Host", "fresh-linkedin-profile-data.p.rapidapi.com" },
+    },
+            };
+            using (var response2 = await client2.SendAsync(request2))
+            {
+                response2.EnsureSuccessStatusCode();
+                var body = await response2.Content.ReadAsStringAsync();
+                ResultLinkedInFollowersDto resultLinkedInFollowersDto = JsonConvert.DeserializeObject<ResultLinkedInFollowersDto>(body);
+                ViewBag.v3 = resultLinkedInFollowersDto.data.followers_count;
+            }
             return View();
         }
     }
